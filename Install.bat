7cc66e9332d6e8c24146de96305d5224079855c9
Bladeage <fageyer@posteo.de>
2024-06-18 12:12:48 +0200

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://github.com/Bladeage/PC-Gaming-Redists-AIO/raw/main/Install.ps1').Content)}"""" | Invoke-Expression"
+powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/Bladeage/PC-Gaming-Redists/main/Install.ps1').Content)}"""" | Invoke-Expression"
 
 pause
 exit /b
